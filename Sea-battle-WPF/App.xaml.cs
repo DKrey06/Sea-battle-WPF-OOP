5c76e403772fc7966105f98f9474b21c34bc0c00
DKrey06 <mrdmitry2006@mail.ru>
2025-11-19 20:23:40 +1000

    Добавьте файлы проекта.

---
+++ Sea-battle-WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sea_battle_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
