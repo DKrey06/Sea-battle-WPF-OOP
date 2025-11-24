160494953380cabec407c6694fce68bbf0a359c4
DKrey06 <mrdmitry2006@mail.ru>
2025-11-24 22:12:17 +1000

    Made the game's start window

---
+++ Sea-battle-WPF/StartWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sea_battle_WPF
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartSinglePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow(false); //false - игра с компьютером
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void StartMultiplayer_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow(true); //true - сетевая игра
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
